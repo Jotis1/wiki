76de8d1f0cdd11ffa2c814553e2e239ec10abdc6
Juan M. Cuellar <juan.cuellar@becallgroup.com>
2026-04-13 12:32:48 +0200

    chore: initialize Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	fonts: [
+		{
+			provider: fontProviders.fontsource(),
+			name: "Geist",
+			cssVariable: "--font-geist-sans",
+		},
+		{
+			provider: fontProviders.fontsource(),
+			name: "Geist Mono",
+			cssVariable: "--font-geist-mono",
+		},
+		{
+			provider: fontProviders.fontsource(),
+			name: "Merriweather",
+			cssVariable: "--font-merriweather",
+		},
+	],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});
